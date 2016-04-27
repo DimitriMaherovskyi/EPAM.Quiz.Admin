342185637b311da02e48e62c0edc06fee9520eac
DemaPavlo <demapavlo@gmail.com>
2016-04-27 12:19:15 +0300

    Added files via upload
    Add text search module with highlight

---
+++ Scripts/app/pages/quiz/search.filter.highligth.js
@@ -0,0 +1,15 @@
+﻿(function (angular) {
+    angular
+        .module("QuizApp")
+        .filter('highlight', function ($sce) {
+            return function (text, phrase) {
+                if (phrase) {
+                    text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span class="highlightedText">$1</span>');
+                }
+
+                return $sce.trustAsHtml(text);
+            }
+        })
+})(angular);
+
+// TODO: add ng-bind-html="student.full_name | highlight: search.text" to student full_name and add file with css style highlighedText
